8ca9caa3008833825437a198fec08da322fe6d2b
Meet Shah <meetdshah007@gmail.com>
2019-02-26 11:14:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var katzDeliLine = [];
 
-function takeANumber(num, name){
-  return `Welcome, ${name}. You are number ${num} in line.`;
+function takeANumber(katzDeliLine, name){
+  katzDeliLine.push(name);
+  return `Welcome, ${name}. You are number ${katzDeliLine.length - 1} in line.`;
 }
 
 function nowServing(current) {
